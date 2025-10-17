bbc7fde2ff91c20f4188ffb5563f03306187ded3
Machrus Ali Wahyudi <89893780+machrusaw@users.noreply.github.com>
2025-10-18 00:49:05 +0700

    Update koneksi.php

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -1,3 +1,12 @@
 <?php
-$koneksi = new mysqli("localhost","root","","dimsum", "3306");
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+$DB_HOST = "mmkf8p.h.filess.io";   // contoh: db-123.filess.cloud
+$DB_USER = "penjualanDimsum_nosegently";
+$DB_PASS = "920822624806e97ffe8131bd387d81d93da3e6e5";
+$DB_NAME = "penjualanDimsum_nosegently";
+$DB_PORT = 3307;                 // biasanya 3306, cek di Filess
+
+$koneksi = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
+$koneksi->set_charset('utf8mb4');
 ?>
